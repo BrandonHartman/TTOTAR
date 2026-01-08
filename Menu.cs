998ccfda1611ffcffd4c1504fcd422a694f0da18
Brandon Hartman <brandonhartman228427@gmail.com>
2026-01-08 13:42:08 -0500

    Updated Menu.cs
    Added the ability to filter through different options on the title screen

--- Menu.cs
+++ Menu.cs
@@ -19,6 +19,28 @@ namespace TTOTAR
                     // .Title("Select an option:")
                     .PageSize(10)
                     .AddChoices(menuOptions));
+            switch (selection)
+            {
+                case "Start New Game":
+                    AnsiConsole.MarkupLine("[green]Starting a new game...[/]");
+                    //StartNewGame();
+                    break;
+                case "Load Game":
+                    AnsiConsole.MarkupLine("[green]Loading game...[/]");
+                    //LoadGame();
+                    break;
+                case "Settings":
+                    AnsiConsole.MarkupLine("[green]Opening settings...[/]");
+                    //OpenSettings();
+                    break;
+                case "Exit":
+                    AnsiConsole.MarkupLine("[green]Exiting the game. Goodbye![/]");
+                    Environment.Exit(0); // Exit the application
+                    break;
+                default:
+                    AnsiConsole.MarkupLine("[red]Invalid selection. Please try again.[/]");
+                    break;
+            }
         }
     }
 }
