88cc859d09f450f81a991b2112da783f00474a61
Brandon Hartman <brandonhartman228427@gmail.com>
2026-01-08 13:18:37 -0500

    Added Menu Options to Title Screen

---
+++ TitleScreen.cs
@@ -0,0 +1,25 @@
+using System;
+using Spectre.Console;
+
+namespace TTOTAR
+{
+    class TitleScreen
+    {
+        public static void ShowGameTitle()
+        {
+            AnsiConsole.Clear();
+            var gameTitle = new FigletText("The Tales of the Ashen Realm")
+                .Centered()
+                .Color(Color.Green);
+            AnsiConsole.Write(gameTitle);
+            var version = new FigletText("RELEASE v0.1")
+            {
+                Color = Color.Green,
+                Justification = Justify.Center
+            };
+            AnsiConsole.Write(new Rule().RuleStyle(Style.Parse("green dim")));
+            AnsiConsole.Write(version);
+            AnsiConsole.Write(new Rule().RuleStyle(Style.Parse("green dim")));
+        }
+    }
+}
