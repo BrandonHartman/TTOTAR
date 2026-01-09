b80ed3ad1e2194177fa503f1afd3f144bf9548a6
Brandon Hartman <brandonhartman228427@gmail.com>
2026-01-09 08:39:22 -0500

    Updated Armor.cs
    Building the blueprint before adding items and placing them on the newly made character.

--- Armor.cs
+++ Armor.cs
@@ -5,9 +5,20 @@ namespace TTOTAR
 {
     public class Armor
     {
+        public ArmorPlacement Slot { get; set;}
+        public required string Name { get; set; }
+
+        public int DefenseBonus { get; set;}
+        
+        public required string Description { get; set; }
+
+        // TODO: Add properties for durability, weight, special effects, etc.
+        public int Weight { get; set; }
+
+        public int Durability { get; set; }
     }
 
-    public enum ArmorType
+    public enum ArmorPlacement
     {
         Head,
         Chest,
