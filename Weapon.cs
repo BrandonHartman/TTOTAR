3f4c251f5799976e80d1a5fb02217a09bbbd7942
Brandon Hartman <brandonhartman228427@gmail.com>
2026-01-09 09:19:14 -0500

    Created Weapon.cs
    Really happy with how this turned out. Will have to tweak it. But good starting point.

---
+++ Weapon.cs
@@ -0,0 +1,84 @@
+using System;
+using Spectre.Console;
+
+namespace TTOTAR
+{
+    public class Weapon
+    {
+        public WeaponType WeaponType { get; set; }
+
+        public DamageType DamageType { get; set; }
+
+        public WeaponRange WeaponRange { get; set; }
+
+        public WeaponCategory WeaponCategory { get; set; }
+
+        public WeaponRarity WeaponRarity { get; set; }
+
+
+        public int Damage { get; set; }
+        public required int MinDamage { get; set; }
+        public required int MaxDamage { get; set; }
+        public required string Name { get; set; }
+
+        public required string Description { get; set; }
+        
+        public int Weight { get; set; }
+
+        public int Durability { get; set; } 
+
+        public int WeaponValue { get; set; }
+
+
+
+    }
+
+    public enum WeaponType
+    {
+        Melee,
+        Ranged,
+        Magic
+    }
+
+    public enum DamageType
+    {
+        Physical,
+        Fire,
+        Ice,
+        Lightning,
+        Poison,
+        Arcane,
+        Slashing,
+        Piercing,
+        Bludgeoning
+
+    }
+    public enum WeaponRange
+    {
+        Short,
+        Medium,
+        Long
+    }
+    public enum WeaponCategory
+    {
+        Axe,
+        Staff,
+        Bow,
+        Crossbow,
+        Wand,
+        Dagger,
+        Sword,
+        Mace,
+        Spear,
+        Hammer,
+        Fist
+    }
+    public enum WeaponRarity
+    {
+        Common,
+        Uncommon,
+        Rare,
+        Epic,
+        Legendary
+    }
+}
